7f8493c91ca465030c68d7aa0e49495b30d3bf77
awxkee <qnljoy@gmail.com>
2023-11-05 17:47:47 +1100

    Upgraded to 1.5.4

--- avifcoillibrary/build.gradle.kts
+++ avifcoillibrary/build.gradle.kts
@@ -15,7 +15,7 @@ afterEvaluate {
             create<MavenPublication>("mavenJava") {
                 groupId = "com.github.awxkee"
                 artifactId = "avif-coder-coil"
-                version = "1.4.0"
+                version = "1.5.4"
                 from(components.findByName("release"))
 //                artifact("androidSourcesJar")
             }
@@ -64,5 +64,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     api("io.coil-kt:coil:2.4.0")
-    api("com.github.awxkee:avif-coder:1.5.3")
+    api("com.github.awxkee:avif-coder:1.5.4")
 }
