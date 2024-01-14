148e59e98993a3252c7390b0b4e39465f13ee65c
awxkee <qnljoy@gmail.com>
2024-01-14 23:43:27 +0000

    Upgraded to 1.5.11

--- avifcoillibrary/build.gradle.kts
+++ avifcoillibrary/build.gradle.kts
@@ -64,5 +64,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     api("io.coil-kt:coil:2.5.0")
-    api("com.github.awxkee:avif-coder:1.5.10")
+    api("com.github.awxkee:avif-coder:1.5.11")
 }
