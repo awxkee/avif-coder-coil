4dc3c0d0f814397d8de8040c81c8b2ef278601e0
awxkee <radzivon.bartoshyk@proton.me>
2024-03-08 12:45:34 +0000

    Upgraded to 1.7.0

--- avifcoillibrary/build.gradle.kts
+++ avifcoillibrary/build.gradle.kts
@@ -63,6 +63,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    api("io.coil-kt:coil:2.5.0")
-    api("com.github.awxkee:avif-coder:1.6.4")
+    api("io.coil-kt:coil:2.6.0")
+    api("com.github.awxkee:avif-coder:1.7.0")
 }
