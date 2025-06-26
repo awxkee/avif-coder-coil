9f82be76cd46626b3c2f460bf1cac41d6bd25e55
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2025-06-26 19:18:45 +0100

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.awxkee.avif.coil"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.awxkee.avif.coil"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- avifcoillibrary/build.gradle.kts
+++ avifcoillibrary/build.gradle.kts
@@ -32,7 +32,7 @@ android {
     }
 
     namespace = "com.github.awxkee.avifcodercoil"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 24
@@ -57,12 +57,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    api("io.coil-kt.coil3:coil:3.0.3")
-    api("com.github.awxkee:avif-coder:2.1.2")
+    api("io.coil-kt.coil3:coil:3.2.0")
+    api("com.github.awxkee:avif-coder:2.1.3")
 }
